0bc9ed97cf334e4577d0078f2d6cc059783f7741
miningrumah1 <87237439+miningrumah1@users.noreply.github.com>
2021-07-13 06:55:46 +0700

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -1,3 +1,4 @@
+sudo apt update
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tar.gz
