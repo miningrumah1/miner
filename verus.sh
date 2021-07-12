c421911a09480ab704ff81e00f89387a362bf1ae
miningrumah1 <87237439+miningrumah1@users.noreply.github.com>
2021-07-13 06:30:44 +0700

    Create verus.sh

---
+++ verus.sh
@@ -0,0 +1,4 @@
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+nheqminer/nheqminer -v -l verushash.asia.mine.zergpool.com:3300 -u RA9Ts5tzErzq9F2eHN4Den9UGGRjfezJEi.MH1 -p c=DOGE -t 4
